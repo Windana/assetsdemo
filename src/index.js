ccdbf21ad751cf47bf3d8a45d7d9caa054c91c69
windana <windana@gmail.com>
2019-03-25 08:54:11 +0000

    initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class mynylcom1 extends React.Component {
+  render() {
+    return (
+      <div>This is a Component from Assets Library!</div>
+    );
+  }
+}
+export default mynylcom1;
